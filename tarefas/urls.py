dc516ab3ea31960ec4385218506e9c5ade0e1cb8
Bandeira98 <lucasbande9898@gmail.com>
2025-05-09 17:09:47 -0300

    version
    campos adicionados: edita, deletar e criar tarefa

--- tarefas/urls.py
+++ tarefas/urls.py
@@ -1,9 +1,11 @@
-
-
 from django.urls import path
-from. import views
+from . import views
 
 urlpatterns = [
-    path('',views.listaTarefa, name='lista_tarefa'),
-   
+    path('', views.listaTarefa, name='lista-tarefa'),
+    path('tarefa/<int:id>', views.tarefaView, name='tarefa-view'),
+    path('novaTarefa/', views.novaTarefa, name='nova-tarefa'),
+    path('edit/<int:id>', views.editTarefa, name='edit-tarefa'),
+    path('delete/<int:id>', views.deleteTarefa, name='delete-tarefa'),
+    
 ]
